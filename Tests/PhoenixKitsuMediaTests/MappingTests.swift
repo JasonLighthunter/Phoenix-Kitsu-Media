cb8d109e31195d3428c161295390f4fa624d95e2
Job Cuppen <jobcuppen@live.nl>
2018-01-08 15:36:39 +0100

    updated MappingTests.swift

--- Tests/PhoenixKitsuMediaTests/MappingTests.swift
+++ Tests/PhoenixKitsuMediaTests/MappingTests.swift
@@ -18,31 +18,31 @@ class MappingTests: XCTestCase {
     ]
   ]
   
-    let validMissingDataJSON: [String : Any] = [
-      "id": "6",
-      "type": "mappings",
-      "links": [
-        "self": "https://kitsu.io/api/edge/mappings/6"
-      ],
-      "attributes": [
-        "externalSite": "myanimelist/anime",
-        "externalId": "4718"
-      ]
+  let validMissingDataJSON: [String : Any] = [
+    "id": "6",
+    "type": "mappings",
+    "links": [
+      "self": "https://kitsu.io/api/edge/mappings/6"
+    ],
+    "attributes": [
+      "externalSite": "myanimelist/anime",
+      "externalId": "4718"
     ]
+  ]
   
-    let validNilDataJSON: [String : Any?] = [
-      "id": "6",
-      "type": "mappings",
-      "links": [
-        "self": "https://kitsu.io/api/edge/mappings/6"
-      ],
-      "attributes": [
-        "createdAt": nil,
-        "updatedAt": nil,
-        "externalSite": "myanimelist/anime",
-        "externalId": "4718"
-      ]
+  let validNilDataJSON: [String : Any?] = [
+    "id": "6",
+    "type": "mappings",
+    "links": [
+      "self": "https://kitsu.io/api/edge/mappings/6"
+    ],
+    "attributes": [
+      "createdAt": nil,
+      "updatedAt": nil,
+      "externalSite": "myanimelist/anime",
+      "externalId": "4718"
     ]
+  ]
   
   let invalidMissingDataJSON: [String : Any] = [
     "id": "6",
@@ -107,57 +107,57 @@ class MappingTests: XCTestCase {
     XCTAssertEqual(mappingAttributes?.externalID, "4718")
   }
   
-    func testMappingValidMissingData() {
-      let json = validMissingDataJSON
-  
-      if JSONSerialization.isValidJSONObject(json as Any) {
-        let data = try? JSONSerialization.data(withJSONObject: json as Any)
-        mapping = try? decoder.decode(Mapping.self, from: data!)
-      } else {
-        mapping = nil
-      }
-      mappingAttributes = mapping?.attributes
-  
-      XCTAssertNotNil(mapping)
-  
-      XCTAssertEqual(mapping?.objectID, "6")
-      XCTAssertEqual(mapping?.type, "mappings")
-  
-      XCTAssertNotNil(mapping?.links)
-  
-      XCTAssertNotNil(mappingAttributes)
-  
-      XCTAssertNil(mappingAttributes?.createdAt)
-      XCTAssertNil(mappingAttributes?.updatedAt)
-      XCTAssertEqual(mappingAttributes?.externalSite, ExternalSiteEnum.myAnimeListAnime)
-      XCTAssertEqual(mappingAttributes?.externalID, "4718")
+  func testMappingValidMissingData() {
+    let json = validMissingDataJSON
+    
+    if JSONSerialization.isValidJSONObject(json as Any) {
+      let data = try? JSONSerialization.data(withJSONObject: json as Any)
+      mapping = try? decoder.decode(Mapping.self, from: data!)
+    } else {
+      mapping = nil
     }
+    mappingAttributes = mapping?.attributes
+    
+    XCTAssertNotNil(mapping)
+    
+    XCTAssertEqual(mapping?.objectID, "6")
+    XCTAssertEqual(mapping?.type, "mappings")
+    
+    XCTAssertNotNil(mapping?.links)
+    
+    XCTAssertNotNil(mappingAttributes)
+    
+    XCTAssertNil(mappingAttributes?.createdAt)
+    XCTAssertNil(mappingAttributes?.updatedAt)
+    XCTAssertEqual(mappingAttributes?.externalSite, ExternalSiteEnum.myAnimeListAnime)
+    XCTAssertEqual(mappingAttributes?.externalID, "4718")
+  }
   
-    func testMappingValidNilData() {
-      let json = validNilDataJSON
-  
-      if JSONSerialization.isValidJSONObject(json as Any) {
-        let data = try? JSONSerialization.data(withJSONObject: json as Any)
-        mapping = try? decoder.decode(Mapping.self, from: data!)
-      } else {
-        mapping = nil
-      }
-      mappingAttributes = mapping?.attributes
-  
-      XCTAssertNotNil(mapping)
-  
-      XCTAssertEqual(mapping?.objectID, "6")
-      XCTAssertEqual(mapping?.type, "mappings")
-  
-      XCTAssertNotNil(mapping?.links)
-  
-      XCTAssertNotNil(mappingAttributes)
-  
-      XCTAssertNil(mappingAttributes?.createdAt)
-      XCTAssertNil(mappingAttributes?.updatedAt)
-      XCTAssertEqual(mappingAttributes?.externalSite, ExternalSiteEnum.myAnimeListAnime)
-      XCTAssertEqual(mappingAttributes?.externalID, "4718")
+  func testMappingValidNilData() {
+    let json = validNilDataJSON
+    
+    if JSONSerialization.isValidJSONObject(json as Any) {
+      let data = try? JSONSerialization.data(withJSONObject: json as Any)
+      mapping = try? decoder.decode(Mapping.self, from: data!)
+    } else {
+      mapping = nil
     }
+    mappingAttributes = mapping?.attributes
+    
+    XCTAssertNotNil(mapping)
+    
+    XCTAssertEqual(mapping?.objectID, "6")
+    XCTAssertEqual(mapping?.type, "mappings")
+    
+    XCTAssertNotNil(mapping?.links)
+    
+    XCTAssertNotNil(mappingAttributes)
+    
+    XCTAssertNil(mappingAttributes?.createdAt)
+    XCTAssertNil(mappingAttributes?.updatedAt)
+    XCTAssertEqual(mappingAttributes?.externalSite, ExternalSiteEnum.myAnimeListAnime)
+    XCTAssertEqual(mappingAttributes?.externalID, "4718")
+  }
   
   func testMappingInvalidMissingData() {
     let json = invalidMissingDataJSON
